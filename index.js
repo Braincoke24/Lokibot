c8d70d019d08dde36c4af9676432ff8d0f3a57e0
Braincoke24 <37015058+Braincoke24@users.noreply.github.com>
2018-03-09 07:41:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const UnitEmbed = require('./src/unitEmbed')
 const PetEmbed = require('./src/petEmbed')
 const Shortcut = require('./src/shortcut')
 const Events = require('./src/events')
+const LilCmds = require('./src/littleCmds')
 const SH1 = require('./src/sh/sh1')
 const SH2 = require('./src/sh/sh2')
 const SH3 = require('./src/sh/sh3')
@@ -30,7 +31,8 @@ var bot = new Discord.Client(),
     unit = new UnitEmbed.unitEmbed(bot, log),
     pet = new PetEmbed.petEmbed(bot, log),
     ss = new Shortcut.shortcut(bot, log),
-    events = new Events.events(bot, log)
+    events = new Events.events(bot, log),
+    lilcmd = new LilCmds.littleCmds(bot, log)
     sh1 = new SH1.sh1(bot, log),
     sh2 = new SH2.sh2(bot, log),
     sh3 = new SH3.sh3(bot, log),
@@ -46,19 +48,24 @@ bot.on('ready', () => {
     log.log('BOT', `Logged in as ${bot.user.username}`)
 })
 /*
-__/\\\\\\\\\\\\\\\__/\\\\\\\\\\\\\\\_____/\\\\\\\\\\\____/\\\\\\\\\\\\\\\_        
- _\///////\\\/////__\/\\\///////////____/\\\/////////\\\_\///////\\\/////__       
-  _______\/\\\_______\/\\\______________\//\\\______\///________\/\\\_______      
-   _______\/\\\_______\/\\\\\\\\\\\_______\////\\\_______________\/\\\_______     
-    _______\/\\\_______\/\\\///////___________\////\\\____________\/\\\_______    
-     _______\/\\\_______\/\\\_____________________\////\\\_________\/\\\_______   
-      _______\/\\\_______\/\\\______________/\\\______\//\\\________\/\\\_______  
-       _______\/\\\_______\/\\\\\\\\\\\\\\\_\///\\\\\\\\\\\/_________\/\\\_______ 
-        _______\///________\///////////////____\///////////___________\///________
+________/\\\\\\\\\__/\\\________/\\\_____/\\\\\\\\\_____/\\\\\\\\\\\\\\\_        
+ _____/\\\////////__\/\\\_______\/\\\___/\\\\\\\\\\\\\__\///////\\\/////__       
+  ___/\\\/___________\/\\\_______\/\\\__/\\\/////////\\\_______\/\\\_______      
+   __/\\\_____________\/\\\\\\\\\\\\\\\_\/\\\_______\/\\\_______\/\\\_______     
+    _\/\\\_____________\/\\\/////////\\\_\/\\\\\\\\\\\\\\\_______\/\\\_______    
+     _\//\\\____________\/\\\_______\/\\\_\/\\\/////////\\\_______\/\\\_______   
+      __\///\\\__________\/\\\_______\/\\\_\/\\\_______\/\\\_______\/\\\_______  
+       ____\////\\\\\\\\\_\/\\\_______\/\\\_\/\\\_______\/\\\_______\/\\\_______ 
+        _______\/////////__\///________\///__\///________\///________\///________
 */
 
 function cmd_say(msg, args) {
     msg.channel.send(args.join(' '))
+    log.log('CMDPARSER', `[USER: ${msg.member.user.tag}] executed command: '${msg.content}'`)
+}
+
+function cmd_ping(msg, args) {
+    lilcmd.ping(msg)
     log.log('CMDPARSER', `[USER: ${msg.member.user.tag}] executed command: '${msg.content}'`)
 }
 
@@ -93,24 +100,34 @@ function cmd_sh(msg, args) {
     args = args[0].split('-')
     if (args[0] == 1) {
         sh1.cmd(msg, args)
+        log.log('CMDPARSER', `[USER: ${msg.member.user.tag}] executed command: '${msg.content}'`)
     } else if (args[1] == 2) {
         sh2.cmd(msg, args)
+        log.log('CMDPARSER', `[USER: ${msg.member.user.tag}] executed command: '${msg.content}'`)
     } else if (args[2] == 3) {
         sh3.cmd(msg, args)
+        log.log('CMDPARSER', `[USER: ${msg.member.user.tag}] executed command: '${msg.content}'`)
     } else if (args[3] == 4) {
         sh4.cmd(msg, args)
+        log.log('CMDPARSER', `[USER: ${msg.member.user.tag}] executed command: '${msg.content}'`)
     } else if (args[4] == 5) {
         sh5.cmd(msg, args)
+        log.log('CMDPARSER', `[USER: ${msg.member.user.tag}] executed command: '${msg.content}'`)
     } else if (args[5] == 6) {
         sh6.cmd(msg, args)
+        log.log('CMDPARSER', `[USER: ${msg.member.user.tag}] executed command: '${msg.content}'`)
     } else if (args[6] == 7) {
         sh7.cmd(msg, args)
+        log.log('CMDPARSER', `[USER: ${msg.member.user.tag}] executed command: '${msg.content}'`)
     } else if (args[7] == 8) {
         sh8.cmd(msg, args)
+        log.log('CMDPARSER', `[USER: ${msg.member.user.tag}] executed command: '${msg.content}'`)
     } else if (args[8] == 9) {
         sh9.cmd(msg, args)
+        log.log('CMDPARSER', `[USER: ${msg.member.user.tag}] executed command: '${msg.content}'`)
     } else if (args[9] == 10) {
         sh10.cmd(msg, args)
+        log.log('CMDPARSER', `[USER: ${msg.member.user.tag}] executed command: '${msg.content}'`)
     } else if (args[10] == 11) {
 
     } else if (args[11] == 12) {
@@ -179,11 +196,12 @@ cmd.addType('EF')
 cmd.setPerms(['417728725933883434', '417774024442118164'], 1)
 
 cmd.register(cmd_say, 'say', ['tell', 'send'], 'Send a message with the bot', config.prefix + 'say <msg>', cmd.type.CHAT, 1)
-    .register(cmd_pet, 'pet', ['tier'], 'Zeigt Info über ein Pet', config.prefix + 'pet', cmd.type.EF, 1)
-    .register(cmd_unit, 'unit', ['einheit'], 'Zeigt Info über eine Einheit', config.prefix + 'unit', cmd.type.EF, 1)
+    .register(cmd_pet, 'pet', ['tier'], 'Zeigt Info über ein Pet', config.prefix + 'pet', cmd.type.EF)
+    .register(cmd_unit, 'unit', ['einheit'], 'Zeigt Info über eine Einheit', config.prefix + 'unit', cmd.type.EF)
     .register(cmd_setStatus, 'setstatus', ['status'], 'Verändert den Status des Bots', config.prefix + 'setstatus', cmd.type.ADMIN, 10)
-    .register(cmd_abk, 'abk', ['abkürzung', 'ss', 'shortcut'], 'Zeigt Abkürzung eines Begriffes bzw. die Bedeutung einer Abkürzung', config.prefix + 'setstatus', cmd.type.EF, 1)
-    .register(cmd_sh, 'sh', ['hdg'], 'Zeigt Lösung eines Levels im Hochland der Geister', config.prefix + 'sh', cmd.type.EF, 1 )
+    .register(cmd_abk, 'abk', ['abkürzung', 'ss', 'shortcut'], 'Zeigt Abkürzung eines Begriffes bzw. die Bedeutung einer Abkürzung', config.prefix + 'setstatus', cmd.type.EF)
+    .register(cmd_sh, 'sh', ['hdg'], 'Zeigt Lösung eines Levels im Hochland der Geister', config.prefix + 'sh', cmd.type.EF)
+    .register(cmd_ping, 'ping', [], 'Gibt *Pong* zurück', config.prefix + 'ping', cmd.type.CHAT)
 
 //REACTROLE SETUP
 reactRole.addGroup('ROLLE')
